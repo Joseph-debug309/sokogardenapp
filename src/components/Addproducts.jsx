107976317fa240db84d5016253d0b0a1b5391e7a
Joseph-debug309 <joemainabz@gmail.com>
2026-03-18 08:32:49 +0300

    update

--- src/components/Addproducts.jsx
+++ src/components/Addproducts.jsx
@@ -4,56 +4,63 @@ import axios from 'axios';
 
 const Addproducts = () => {
 
-  // Introduce the hook
+  // Introduce the hook (2)
   const [product_name, setProductName] = useState("");
   const [product_description, setProductDescription] = useState("");
   const [product_cost, setProductCost] = useState("");
   const [product_photo, setProductPhoto] = useState("");
 
+  //create a reference
+
   const imageInputRef = useRef(null)
 
 
-  // declare the additional hool to manage the states of the application
+  // declare the additional hool to manage the states of the application (3)
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
 
-  // ⦁	Come up with the function that will handle the submit function
+  // ⦁	Come up with the function that will handle the submit function (4)
   const handleSubmit = async (e) =>{
     // Prevent the site from reloading
     e.preventDefault()
 
-    // Set a loading hook activating it
+    // Set a loading hook activating it (5)
     setLoading(true)
 
      
-
+// WE create the try and catch block (6)
     try{
-      // create a form data
+      // create a form data (7)
       const formdata = new FormData()
 
-      // append the details to the form data created
+      // append the details to the form data created (8)
       formdata.append("product_name", product_name)
       formdata.append("product_description", product_description)
       formdata.append("product_cost", product_cost)
       formdata.append("product_photo", product_photo)
 
-      // interact with axios to access the method post
+      // interact with axios to access the method post (9)
       const response = await axios.post("http://josephdebug.alwaysdata.net/api/add_product", formdata)
 
-      // set the loading hook back to default
+      // set the loading hook back to default (10)
       setLoading(false)
 
-      // Update the success hook with a message
+      // Update the success hook with a message (11)
       setSuccess(response.data.message)
 
-      // clearing the hooks 
+      // clearing the hooks (12)
       setProductName("");
       setProductDescription("");
       setProductCost("");
       imageInputRef.current.value= "";
       // setProductPhoto("");
 
+      e.target.reset()
+
+      setTimeout(() => {
+        setSuccess("");
+      }, 5000);
   }
 
   catch(error){
@@ -67,6 +74,7 @@ const Addproducts = () => {
 
   
 
+//  ⦁	Creating the UI i.e product_details(1)
 
   return (
     <div className='row justify-content-center mt-4'>
--- src/components/Getproducts.jsx
+++ src/components/Getproducts.jsx
@@ -1,10 +1,91 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Loader from './Loader';
 
 const Getproducts = () => {
+
+  //	We create a hook to get the products (1)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // below we specifiy the image base url
+  const img_url = "http://josephdebug.alwaysdata.net/static/images/"
+
+  // Create a function to help you fetch products from your API (2)
+  const fetchProducts = async() =>{
+
+    // come up with the try and catch block (3)
+    try{
+      // Update the loading hook(4)
+      setLoading(true)
+
+
+      // Interact with your endpoint for fetching the products(5)
+      const response = await axios.get("http://josephdebug.alwaysdata.net/api/get_products")
+
+      // Update the products hook withthe response from the API(6)
+      setProducts(response.data)
+
+      // Set the loading hook back to default (7)
+      setLoading(false)
+
+    }
+    catch(error){
+      // If there is an error(8)
+      // set the loading hook back to default
+      setLoading(false)
+
+
+      // Update the error hook with a message
+      setError(error.message)
+
+    }
+  }
+
+
+
+  // We shall use the useEffect hook that automatically re-render new features incase of any changes.
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  // console.log(products)
+
+
   return (
-    <div>
-      <h1>Welcome to the get products page</h1>
+    <div className='row' >
+      <h3 className='text-primary' >Available Products</h3>
+
+      { loading && <Loader/> }
+      
+      <h4 className='text-danger' >{error}</h4>
+
+      {/* map thee products fetched from the api to the user interface */}
+
+      {products.map((product) => (
+        <div className="col-md-3 justify-content-center mb-3">
+          <div className="card shadow">
+          <img src={img_url + product.product_photo} 
+          alt="product name" 
+          className='product_img mt-3' />
+
+            <div className="card-body">
+            <h5 className="text-secondary">{product.product_name}</h5>
+
+            <p className="text-dark"> {product.product_description.slice(0, 100)}... </p>
+
+            <h4 className="text-warning"> Kes {product.product_cost} </h4>
+
+            </div>
+          </div>
+        </div>
+      
+      )  )}
+
+      
     </div>
+    
   )
 }
 
